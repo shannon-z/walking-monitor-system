6c475452758f7a1be81d3a9cfe7a11f7047d17cb
zhangyong.lc01 <zhangyong.lc01@inspur.com>
2020-05-07 17:52:29 +0800

    提交修改

--- src/components/userHealth/ExerciseMonitor.vue
+++ src/components/userHealth/ExerciseMonitor.vue
@@ -1,196 +1,205 @@
 <template>
-    <div>
-
-    </div>
+  <div>
+    {{ msg }}
+  </div>
 </template>
 
 <script>
-
-    export default {
-        name: "ExerciseMonitor",
-        data() {
-            return {
-
-            }
-        },
-        mounted() {
-
-
-            let ros = new this.$roslib.Ros({
-                url: 'ws://192.168.246.142:9090'
-            });
-            ros.on('connection', function () {
-                console.log('Connected to websocket server.');
-            });
-            ros.on('error', function (error) {
-                console.log('Error connecting to websocket server: ', error);
-            });
-
-            ros.on('close', function () {
-                console.log('Connection to websocket server closed.');
-            });
-            let vel = new this.$roslib.Topic({
-                ros: ros,
-                name: '/people_velocity',
-                messageType: 'std_msgs/String'
-            });
-            let strs = new Array();
-
-            vel.subscribe(function (message) {
-                console.log('Received message on ' + vel.name + ': ' + message.data);
-                document.getElementById("bar").innerHTML = message.data;
-                strs = message.data.split(",");
-                // $.ajax({
-                //     type: 'POST',
-                //     url: '/Lidar/',
-                //     dataType: 'json',
-                //     data: {
-                //         'person_id': strs[0],
-                //         'step_length': strs[3],
-                //         'person_left_vel': strs[4],
-                //         'person_right_vel': strs[5],
-                //         'stamp': strs[6]
-                //     },
-                //     async: false,
-                //     success: function (data) {
-                //     }
-                // });
-
-
-                // listener.unsubscribe();
-            });
-            let loc = new this.$roslib.Topic({
-                ros: ros,
-                name: '/people_location',
-                messageType: 'std_msgs/String'
-            });
-            loc.subscribe(function (message) {
-                console.log('Received message on ' + loc.name + ': ' + message.data);
-                // listener.unsubscribe();
-            });
-
-
-
-
-
-
-            // let val = 0.7;
-            // const option = {
-            //     backgroundColor: "#062a44",
-            //     series: [{
-            //         type: 'gauge',
-            //         radius: '60%',
-            //         min: 0,
-            //         max: 1,
-            //         startAngle: 225,
-            //         endAngle: -45,
-            //         axisLine: {
-            //             show: false,
-            //             lineStyle: {
-            //                 width: 0,
-            //                 color: [1, '#fff']
-            //             }
-            //         }, //仪表盘轴线
-            //         axisTick: {
-            //             show: true,
-            //             splitNumber: 9,
-            //             lineStyle: {
-            //                 color: '#fff',
-            //             },
-            //             length: -8
-            //         }, //刻度样式
-            //         splitLine: {
-            //             show: true,
-            //             length: 5,
-            //             lineStyle: {
-            //                 color: '#fff'
-            //             }
-            //         }, //分隔线样式
-            //         detail: {
-            //             show: false
-            //         },
-            //         pointer: {
-            //             show: false
-            //         },
-            //         axisLabel: {
-            //             show: true,
-            //             color: '#fff',
-            //         }
-            //     },
-            //         {
-            //             type: 'gauge',
-            //             radius: '45%',
-            //             center: ['50%', '50%'],
-            //             startAngle: 225,
-            //             endAngle: -45,
-            //             axisLine: {
-            //                 show: true,
-            //                 lineStyle: {
-            //                     width: 10,
-            //                     color: [
-            //                         [0, '#FFC700'],
-            //                         [val / 1, '#FFC700'], // 数据
-            //                         [1, '#999999']
-            //                     ]
-            //
-            //                 }
-            //             },
-            //             //分隔线样式。
-            //             splitLine: {
-            //                 show: false,
-            //                 length: -20,
-            //                 // lineStyle: {
-            //                 //     color: '#fff'
-            //                 // }
-            //             }, //分隔线样式
-            //             axisLabel: {
-            //                 show: false
-            //             },
-            //             axisTick: {
-            //                 show: false,
-            //                 splitNumber: 9,
-            //                 // lineStyle: {
-            //                 //     color: '#fff',
-            //                 // },
-            //                 length: -8
-            //             }, //刻度样式
-            //             pointer: {
-            //                 show: false
-            //             },
-            //             title: {
-            //                 show: true,
-            //                 offsetCenter: [0, '-26%'], // x, y，单位px
-            //                 textStyle: {
-            //                     color: '#fff',
-            //                     fontSize: 16
-            //                 }
-            //             },
-            //             //仪表盘详情，用于显示数据。
-            //             detail: {
-            //                 show: true,
-            //                 offsetCenter: [0, '16%'],
-            //                 color: '#ffffff',
-            //                 formatter: function (params) {
-            //                     return params + 'm/s'
-            //                 },
-            //                 textStyle: {
-            //                     fontSize: 22
-            //                 }
-            //             },
-            //             data: [{
-            //                 name: "当前行走速度",
-            //                 value: val
-            //             }]
-            //         }
-            //     ]
-            // };
-        },
-        methods: {
-
-        }
+export default {
+  name: 'ExerciseMonitor',
+  data () {
+    return {
+      msg: ''
     }
+  },
+  mounted () {
+    let self = this
+
+    let ros = new this.$roslib.Ros({
+      url: 'ws://192.168.246.143:9090'
+    })
+    ros.on('connection', function () {
+      console.log('Connected to websocket server.')
+    })
+    ros.on('error', function (error) {
+      console.log('Error connecting to websocket server: ', error)
+    })
+
+    ros.on('close', function () {
+      console.log('Connection to websocket server closed.')
+    })
+    let vel = new this.$roslib.Topic({
+      ros: ros,
+      name: '/people_velocity',
+      messageType: 'std_msgs/String'
+    })
+    let strs = new Array()
+
+    vel.subscribe(function (message) {
+      console.log('Received message on ' + vel.name + ': ' + message.data)
+      // document.getElementById("bar").innerHTML = message.data;
+      self.msg = message.data
+      strs = message.data.split(',')
+      this.$axios({
+        url: '/Lidar/',
+        method: 'post',
+        responseType: 'json', // 默认的
+        data: {
+          person_id: strs[0],
+          step_length: strs[3],
+          person_left_vel: strs[4],
+          person_right_vel: strs[5],
+          stamp: strs[6]
+        },
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      // .then(function (response) {
+      //     console.log(response)
+      //     console.log(response.data)
+      // })
+      // $.ajax({
+      //     type: 'POST',
+      //     url: '/Lidar/',
+      //     dataType: 'json',
+      //     data: {
+      //         'person_id': strs[0],
+      //         'step_length': strs[3],
+      //         'person_left_vel': strs[4],
+      //         'person_right_vel': strs[5],
+      //         'stamp': strs[6]
+      //     },
+      //     async: false,
+      //     success: function (data) {
+      //     }
+      // });
+
+      // listener.unsubscribe();
+    })
+    let loc = new this.$roslib.Topic({
+      ros: ros,
+      name: '/people_location',
+      messageType: 'std_msgs/String'
+    })
+    loc.subscribe(function (message) {
+      console.log('Received message on ' + loc.name + ': ' + message.data)
+      // listener.unsubscribe();
+    })
+
+    // let val = 0.7;
+    // const option = {
+    //     backgroundColor: "#062a44",
+    //     series: [{
+    //         type: 'gauge',
+    //         radius: '60%',
+    //         min: 0,
+    //         max: 1,
+    //         startAngle: 225,
+    //         endAngle: -45,
+    //         axisLine: {
+    //             show: false,
+    //             lineStyle: {
+    //                 width: 0,
+    //                 color: [1, '#fff']
+    //             }
+    //         }, //仪表盘轴线
+    //         axisTick: {
+    //             show: true,
+    //             splitNumber: 9,
+    //             lineStyle: {
+    //                 color: '#fff',
+    //             },
+    //             length: -8
+    //         }, //刻度样式
+    //         splitLine: {
+    //             show: true,
+    //             length: 5,
+    //             lineStyle: {
+    //                 color: '#fff'
+    //             }
+    //         }, //分隔线样式
+    //         detail: {
+    //             show: false
+    //         },
+    //         pointer: {
+    //             show: false
+    //         },
+    //         axisLabel: {
+    //             show: true,
+    //             color: '#fff',
+    //         }
+    //     },
+    //         {
+    //             type: 'gauge',
+    //             radius: '45%',
+    //             center: ['50%', '50%'],
+    //             startAngle: 225,
+    //             endAngle: -45,
+    //             axisLine: {
+    //                 show: true,
+    //                 lineStyle: {
+    //                     width: 10,
+    //                     color: [
+    //                         [0, '#FFC700'],
+    //                         [val / 1, '#FFC700'], // 数据
+    //                         [1, '#999999']
+    //                     ]
+    //
+    //                 }
+    //             },
+    //             //分隔线样式。
+    //             splitLine: {
+    //                 show: false,
+    //                 length: -20,
+    //                 // lineStyle: {
+    //                 //     color: '#fff'
+    //                 // }
+    //             }, //分隔线样式
+    //             axisLabel: {
+    //                 show: false
+    //             },
+    //             axisTick: {
+    //                 show: false,
+    //                 splitNumber: 9,
+    //                 // lineStyle: {
+    //                 //     color: '#fff',
+    //                 // },
+    //                 length: -8
+    //             }, //刻度样式
+    //             pointer: {
+    //                 show: false
+    //             },
+    //             title: {
+    //                 show: true,
+    //                 offsetCenter: [0, '-26%'], // x, y，单位px
+    //                 textStyle: {
+    //                     color: '#fff',
+    //                     fontSize: 16
+    //                 }
+    //             },
+    //             //仪表盘详情，用于显示数据。
+    //             detail: {
+    //                 show: true,
+    //                 offsetCenter: [0, '16%'],
+    //                 color: '#ffffff',
+    //                 formatter: function (params) {
+    //                     return params + 'm/s'
+    //                 },
+    //                 textStyle: {
+    //                     fontSize: 22
+    //                 }
+    //             },
+    //             data: [{
+    //                 name: "当前行走速度",
+    //                 value: val
+    //             }]
+    //         }
+    //     ]
+    // };
+  },
+  methods: {}
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
